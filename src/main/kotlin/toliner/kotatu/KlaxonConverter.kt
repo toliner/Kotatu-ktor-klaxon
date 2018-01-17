967cc814c33e5d8c256f91afe32c74919a5ef4bc
toliner <toliner.rkt@gmail.com>
2018-01-17 20:07:31 +0900

    Register KtorConverter to ContentNegotiation.Configuration

--- src/main/kotlin/toliner/kotatu/KlaxonConverter.kt
+++ src/main/kotlin/toliner/kotatu/KlaxonConverter.kt
@@ -7,14 +7,14 @@ import io.ktor.content.IncomingContent
 import io.ktor.content.TextContent
 import io.ktor.content.readText
 import io.ktor.features.ContentConverter
+import io.ktor.features.ContentNegotiation
 import io.ktor.features.suitableCharset
 import io.ktor.http.ContentType
 import io.ktor.http.withCharset
 import io.ktor.pipeline.PipelineContext
 import io.ktor.request.ApplicationReceiveRequest
 
-class KlaxonConverter : ContentConverter {
-    private val klaxon = Klaxon()
+class KlaxonConverter(private val klaxon: Klaxon = Klaxon()) : ContentConverter {
 
     suspend override fun convertForSend(context: PipelineContext<Any, ApplicationCall>, contentType: ContentType, value: Any): Any? {
         return TextContent(klaxon.toJsonString(value), contentType.withCharset(context.call.suitableCharset()))
@@ -25,4 +25,9 @@ class KlaxonConverter : ContentConverter {
         return klaxon.fromJsonObject(klaxon.parseJsonObject((request.value as? IncomingContent ?: return null).readText().reader()), request.type.javaObjectType, request.type)
     }
 
+}
+
+fun ContentNegotiation.Configuration.klaxon(block: Klaxon.() -> Unit) {
+    val converter = KlaxonConverter(Klaxon().apply(block))
+    register(ContentType.Application.Json, converter)
 }
